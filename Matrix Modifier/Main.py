e3cde7131b35c3f2eec012f866380567feb1bc97
VoidAutomata <82106210+VoidAutomata@users.noreply.github.com>
2022-01-17 13:38:05 -0500

    Solve for Reduced Row
    Tests function usage to facilitate converting a matrix to reduced row echelon form.

--- Matrix Modifier/Main.py
+++ Matrix Modifier/Main.py
@@ -1,7 +1,7 @@
 import array
 import numpy as np
 
-matrix = np.array([[1, -1, -1, 0, -2, 9], [3, 2, -8, 5, 9,-43], [2, -3, -1, -1, -6, 28]])
+matrix = np.array([[1, 2, -2, 7], [-1, 0, 4, -13], [1, -1, -4, 12]])
 #print(matrix[0,0]);
 
 def multiplyAdd (row1, row2, co):
@@ -10,6 +10,21 @@ def multiplyAdd (row1, row2, co):
     #co is "coefficient", or multiplicative operand
     matrix[row2] += matrix[row1]*co
 
+def swapRow (row1, row2):
+    matrix[[row1, row2]] = matrix[[row2, row1]]
 
-multiplyAdd(0, 1, -1)
+def div (row, co):
+    matrix[row] = matrix[row] / co
+
+print("to Row Echelon form:")
+#multiplyAdd(0, 1, -1)
+multiplyAdd(0, 1, 1)
+multiplyAdd(0, 2, -1)
+div(1, 2)
+multiplyAdd(1, 2, 3)
+print(matrix)
+print("to Reduced Row Echelon form:")
+multiplyAdd(1, 0, -2)
+multiplyAdd(2, 1, -1)
+multiplyAdd(2, 0, 4)
 print(matrix)
