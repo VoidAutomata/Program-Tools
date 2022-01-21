4c59a2469284909d1e8dbed9557b7578b2145846
VoidAutomata <82106210+VoidAutomata@users.noreply.github.com>
2022-01-21 09:44:27 -0500

    More operations
    Used more operations
    -dot product
    -multiply
    -add
    -subtract
    -transpose

--- Matrix Modifier/Main.py
+++ Matrix Modifier/Main.py
@@ -1,7 +1,8 @@
 import array
 import numpy as np
 
-matrix = np.array([[1, 2, -2, 7], [-1, 0, 4, -13], [1, -1, -4, 12]])
+A1 = np.array([[-1, -2, 3], [4, 0, -4]])
+A2 = np.array([[4, -3], [-5, 2], [-1, -2]])
 #print(matrix[0,0]);
 
 def multiplyAdd (row1, row2, co):
@@ -10,21 +11,22 @@ def multiplyAdd (row1, row2, co):
     #co is "coefficient", or multiplicative operand
     matrix[row2] += matrix[row1]*co
 
-def swapRow (row1, row2):
+def swap (row1, row2):
     matrix[[row1, row2]] = matrix[[row2, row1]]
 
 def div (row, co):
     matrix[row] = matrix[row] / co
 
 print("to Row Echelon form:")
-#multiplyAdd(0, 1, -1)
-multiplyAdd(0, 1, 1)
-multiplyAdd(0, 2, -1)
-div(1, 2)
-multiplyAdd(1, 2, 3)
-print(matrix)
-print("to Reduced Row Echelon form:")
-multiplyAdd(1, 0, -2)
-multiplyAdd(2, 1, -1)
-multiplyAdd(2, 0, 4)
-print(matrix)
+#swap(2, 0)
+#div(0, 2)
+#multiplyAdd(0, 2, -2)
+
+A1 = np.transpose(A1)
+#print(np.add(A1, A2))
+#print(np.dot(A1, A2))
+print(np.subtract(np.multiply(A1, 2), A2))
+#multiplyAdd(1, 0, -2)
+
+
+
