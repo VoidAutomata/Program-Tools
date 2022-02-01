2973d3d87b39ac39742e4a1445240c37c73ca2e8
VoidAutomata <82106210+VoidAutomata@users.noreply.github.com>
2022-02-01 13:44:31 -0500

    Secondary Functions
    Added secondary functions for second matrix (augmented)

--- Matrix Modifier/Main.py
+++ Matrix Modifier/Main.py
@@ -1,31 +1,59 @@
 import array
 import numpy as np
+from fractions import Fraction
 
-A1 = np.array([[-1, -2, 3], [4, 0, -4]])
-A2 = np.array([[4, -3], [-5, 2], [-1, -2]])
+A1 = np.array([[5, 5, 5], [-1, -1, 0], [1, 2, -2]])
+A2 = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
 #print(matrix[0,0]);
 
-def multiplyAdd (row1, row2, co):
+def multiplyAdd (matrix, row1, row2, co):
     #row1 is operator
     #row2 is operated
     #co is "coefficient", or multiplicative operand
     matrix[row2] += matrix[row1]*co
 
-def swap (row1, row2):
+def multiplyAdd2 (matrix, matrix2, row1, row2, co):
+    #row1 is operator
+    #row2 is operated
+    #co is "coefficient", or multiplicative operand
+    matrix[row2] += matrix[row1] * co
+    matrix2[row2] += matrix[row1] * co
+
+def swap (matrix, row1, row2):
     matrix[[row1, row2]] = matrix[[row2, row1]]
 
-def div (row, co):
+def swap2 (matrix, matrix2, row1, row2):
+    matrix2[[row1, row2]] = matrix2[[row2, row1]]
+    matrix[[row1, row2]] = matrix[[row2, row1]]
+
+def div (matrix, row, co):
     matrix[row] = matrix[row] / co
 
-print("to Row Echelon form:")
-#swap(2, 0)
-#div(0, 2)
+def div (matrix, matrix2, row, co):
+    matrix[row] = matrix[row] / co
+    matrix2[row] = matrix2[row] / co
+
+
+#print("to Row Echelon form:")
+print(np.linalg.inv(A1))
+swap2(A1, A2, 0, 2)
+multiplyAdd(A1, 0, 1, 1)
+multiplyAdd(A1, 0, 2, -5)
+multiplyAdd(A1, 1, 2, 5)
+div(A1, 2, 5)
+multiplyAdd(A1, 2, 1, 2)
+#multiplyAdd2(A1, A2, 0, 1, -3)
+#div(A1, 1, 3)
+
+
+
 #multiplyAdd(0, 2, -2)
 
-A1 = np.transpose(A1)
 #print(np.add(A1, A2))
 #print(np.dot(A1, A2))
-print(np.subtract(np.multiply(A1, 2), A2))
+print("//")
+print(A1)
+#print(A2)
 #multiplyAdd(1, 0, -2)
 
 
